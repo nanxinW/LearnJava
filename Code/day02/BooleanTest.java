ca42d0e6caf29651804de1c2266945359834e375
nanxinW <54395562+nanxinW@users.noreply.github.com>
2020-03-12 17:10:10 +0800

    Create BooleanTest.java

---
+++ Code/day02/BooleanTest.java
@@ -0,0 +1,12 @@
+/*
+	1、在java语言中Boolean类型只有两个数值，没有其他值：true和false。
+*/
+
+public class BooleanTest
+{
+	public static void main(String[] args)
+	{
+		boolean sex = true;
+		System.out.println(sex);
+	}
+}
